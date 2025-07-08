01cf1f9a2bfcd6bbc5b1cf9ae12ff64844fdc284
Jules Coubetergues <julescoubetergues@icloud.com>
2025-07-08 11:55:21 +0200

    Test

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,6 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-if (screen.orientation && screen.orientation.lock) {
-    screen.orientation.lock('portrait')
-        .catch(err => console.warn('Impossible de lock l’orientation:', err));
-}
-
 class StudentActivityApp {
     constructor() {
         this.students = [];
